74461cb77d124967c36c3bb45a7c483e1c84d5c2
Rajesh000730 <rajeshrachit09@gmail.com>
2023-02-24 21:59:52 +0530

    padlindrome

--- palindrome.cpp
+++ palindrome.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 bool ispalindrome(string s){
+  
   int n = s.size();
   int l = 0;
   int h = n-1;
+  //check if both pointers have same characters until j crosses i
   while(i < l){
      if(s[i] != s[h]) return false;     
   }

259b5e842309a9b1f0dbe75f48808530159f1a68
Rajesh000730 <rajeshrachit09@gmail.com>
2023-02-24 22:00:09 +0530

    rotateMatrix

--- rotateMatrix.cpp
+++ rotateMatrix.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
-void rotateMatrix(vector<vector<int>> &v){
-    
+void rotateMatrix(vector<vector<int>> &matrix){
+   for(int i = 0; i < n; i++){
+      for(int j = i + 1; j < n; j++ ){
+          int temp = matrix[i][j];
+          matrix[i][j] = matrix[j][i];
+          matrix[j][i] = temp; 
+      }
+   }    
+
+   int ind = n - 1;
+   for(int i = 0; i < n; i++){
+      for(int j = 0; j < n/2; j++){
+          
+      }
+   }
 }

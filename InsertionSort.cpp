5723b4b602de3caf71286f395f3ce4259338c174
Rajesh000730 <rajeshrachit09@gmail.com>
2023-02-24 21:57:47 +0530

    insertion sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -8,12 +8,16 @@ void InsertionSort(vector<int> &arr){
   int n = arr.size();
   int current;
   for(int i = 1; i < n; i++){
+      // storing arr[i] to current element
       current = arr[i];
       j = i-1;
+      // comparing if arr[j] is greater than current until j is greater than 0
       while(j > 0 && arr[j] > current){
+         // copy arr[j + 1] to arr[j]
          arr[j] = arr[j + 1];
          j--;
       }
+      // copy current to arr[j + 1]
       arr[j + 1] = current;
   }
 

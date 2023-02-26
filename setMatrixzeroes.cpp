6ff2af97a687fd71c8bd93e1eac596c18efccd10
Rajesh000730 <rajeshrachit09@gmail.com>
2023-02-26 11:49:08 +0530

    set matrix zeroes

---
+++ setMatrixzeroes.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+      vector<pair<int,int>>p;
+      for(int i = 0; i < matrix.size(); i++){
+          for(int j = 0; j < matrix[0].size();j++){
+              if(matrix[i][j] == 0){
+                 p.push_back({i,j});
+              }
+          }
+      }
+      vector<int>row;
+      vector<int>col;
+      for(auto x : p){
+          if(find(row.begin(),row.end(),x.first) == row.end())
+          {
+            for(int j = 0; j < matrix[0].size();j++){
+                matrix[x.first][j] = 0;
+            }
+            row.push_back(x.first);
+          }
+          if(find(col.begin(), col.end(), x.second)==col.end())
+          {
+            for(int j = 0; j < matrix.size();j++){
+                matrix[j][x.second] = 0;
+            }
+            col.push_back(x.second);
+          }
+      }
+    }
+};

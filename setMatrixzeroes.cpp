ab5723221091d6cd7073ed302a8b27955a723bf6
Rajesh000730 <rajeshrachit09@gmail.com>
2023-12-03 05:37:36 +0530

    setmatrixzeroes

--- setMatrixzeroes.cpp
+++ setMatrixzeroes.cpp
@@ -6,28 +6,22 @@ public:
       vector<pair<int,int>>p;
       vector<int>row;
       vector<int>col;
-      for(int i = 0; i < matrix.size(); i++){
-          for(int j = 0; j < matrix[0].size();j++){
+      int m = matrix.size();
+      int n = matrix[0].size();
+      for(int i = 0; i < m; i++){
+          for(int j = 0; j < n ;j++){
               if(matrix[i][j] == 0){
-                 p.push_back({i,j});
+                row.emplace_back(i);
+                col.emplace_back(j);
               }
           }
       }
-      for(auto x : p){
-          if(find(row.begin(),row.end(),x.first) == row.end())
-          {
-            for(int j = 0; j < matrix[0].size();j++){
-                matrix[x.first][j] = 0;
-            }
-            row.push_back(x.first);
-          }
-          if(find(col.begin(), col.end(), x.second)==col.end())
-          {
-            for(int j = 0; j < matrix.size();j++){
-                matrix[j][x.second] = 0;
-            }
-            col.push_back(x.second);
-          }
+      for(int i =0; i < m; i++){
+        for(int j = 0; j < n; j++){
+          if(row[i] == 1 || col[j] == 1){
+            matrix[i][j] = 0;
+        }
       }
+    }
     }
 };
